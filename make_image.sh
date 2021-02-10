28403d220fc5d7cb828a8c4010cb12200ef14ee4
vernon2gh <vernon2gm@gmail.com>
2021-02-10 11:19:45 +0800

    Add: make_image.sh

---
+++ make_image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+useage()
+{
+	echo "[useage]: $0 <ext2/ext3/ext4>"
+}
+
+if [ ! $1 ]; then
+	useage
+	exit
+fi
+
+FSTYPE=$1
+IMAGE=rootfs.$FSTYPE
+
+rm -rf $IMAGE
+
+mkdir -p tmpfs
+dd if=/dev/zero of=$IMAGE bs=1M count=32
+mkfs.$FSTYPE -F $IMAGE
+sudo mount -t $FSTYPE -o loop $IMAGE ./tmpfs
+
+sudo cp -r submodule/busybox/output/_install/* tmpfs/
+sudo mkdir tmpfs/dev
+sudo mknod tmpfs/dev/console c 5 1
+sudo mknod tmpfs/dev/tty2 c 4 2
+sudo mknod tmpfs/dev/tty3 c 4 3
+sudo mknod tmpfs/dev/tty4 c 4 4
+sudo cp -r rootfs/*  tmpfs/
+
+sudo umount tmpfs
+rm -rf tmpfs
