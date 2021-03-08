31e3d68970f56777e5b67be68241fb3fe6e5b5ce
vernon2gh <vernon2gm@gmail.com>
2021-03-08 15:42:34 +0800

    Fixed: docker have not sudo cmd

--- make_image.sh
+++ make_image.sh
@@ -13,20 +13,26 @@ fi
 FSTYPE=$1
 IMAGE=rootfs.$FSTYPE
 
+if [ -e "/.dockerenv" ]; then
+	SUDO=
+else
+	SUDO=sudo
+fi
+
 rm -rf rootfs.*
 
 mkdir -p tmpfs
 dd if=/dev/zero of=$IMAGE bs=1M count=32
 mkfs.$FSTYPE -F $IMAGE
-sudo mount -t $FSTYPE -o loop $IMAGE ./tmpfs
+$SUDO mount -t $FSTYPE -o loop $IMAGE ./tmpfs
 
-sudo cp -r submodule/busybox/output/_install/* tmpfs/
-sudo mkdir tmpfs/dev
-sudo mknod tmpfs/dev/console c 5 1
-sudo mknod tmpfs/dev/tty2 c 4 2
-sudo mknod tmpfs/dev/tty3 c 4 3
-sudo mknod tmpfs/dev/tty4 c 4 4
-sudo cp -r rootfs/*  tmpfs/
+$SUDO cp -r submodule/busybox/output/_install/* tmpfs/
+$SUDO mkdir tmpfs/dev
+$SUDO mknod tmpfs/dev/console c 5 1
+$SUDO mknod tmpfs/dev/tty2 c 4 2
+$SUDO mknod tmpfs/dev/tty3 c 4 3
+$SUDO mknod tmpfs/dev/tty4 c 4 4
+$SUDO cp -r rootfs/*  tmpfs/
 
-sudo umount tmpfs
+$SUDO umount tmpfs
 rm -rf tmpfs
